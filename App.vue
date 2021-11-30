be8f59699dfa4d6ef5a0036b959ede323bc5310b
邓健 <JDeng@senparc.com>
2021-11-30 17:56:23 +0800

    demo init

---
+++ App.vue
@@ -0,0 +1,24 @@
+<script>
+	import userMessage from '@/store/userMessage.js';
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			if (!userMessage.state.loginStatus) {
+				console.log('未登录，onLaunch 拦截去登录');
+				uni.reLaunch({
+					url: '/pages/login/login'
+				})
+			}
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
